8bf5c9cd4c0be12dba602c318c8800988ec61de9
patgpt <doitformurphy@gmail.com>
2025-01-26 14:51:10 -0500

    chore: :pencil2:

--- eslint.config.ts
+++ eslint.config.ts
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  {
+    rules: {
+      "react/jsx-no-literals": "error",
+    },
+  },
 ];
 
 export default eslintConfig;
